69cd1ea9f4d89e2c7bcd0376aad0784c429a1c6c
Sattwik Biswas (Industrial SA388674 Engineering Services (ISA388674ES)) <SA388674@wipro.com>
2019-01-15 16:06:56 +0530

    changes

--- www/ReadNFC.js
+++ www/ReadNFC.js
@@ -1,8 +1,8 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'ReadNFC', 'coolMethod', [arg0]);
-};
+// exports.coolMethod = function (arg0, success, error) {
+//     exec(success, error, 'ReadNFC', 'coolMethod', [arg0]);
+// };
 
 module.exports.readNFC = function(arg0, success, error) {
     exec(success, error, 'ReadNFC', 'readNFC', [arg0]);
