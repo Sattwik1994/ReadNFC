f2ce1acf12cdcfa83dd93303f1b81bedcf4972f4
Sattwik Biswas (Industrial SA388674 Engineering Services (ISA388674ES)) <SA388674@wipro.com>
2019-01-16 14:58:52 +0530

    changes

--- src/android/ReadNFC.java
+++ src/android/ReadNFC.java
@@ -7,6 +7,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.os.Bundle;
+import android.os.Parcelable;
+
 /**
  * This class echoes a string called from JavaScript.
  */
@@ -19,7 +29,7 @@ public class ReadNFC extends CordovaPlugin {
         //     this.coolMethod(message, callbackContext);
         //     return true;
         // }
-        if(action.equals("readNFC")) {
+        if (action.equals("readNFC")) {
             this.readNFC(callbackContext);
             return true;
         }
@@ -33,12 +43,58 @@ public class ReadNFC extends CordovaPlugin {
     //         callbackContext.error("Expected one non-empty string argument.");
     //     }
     // }
-    
+
     private void readNFC(CallbackContext callbackContext) {
         try {
-            callbackContext.success("This plugin reads NFC cards");
-        } catch(Exception e) {
+            nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+            if (nfcAdapter == null) {
+                finish();
+            }
+            this.readFromIntent(getIntent());
+            pendingIntent = PendingIntent.getActivity(this, 0,
+                    new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+            IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+            tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+            
+        } catch (Exception e) {
             callbackContext.error("Something went wrong.");
         }
+    }
+
+    private void readFromIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action) || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] msgs = null;
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            }
+            this.buildTagViews(msgs);
+        }
+    }
+
+    private void buildTagViews(NdefMessage[] msgs) {
+        if (msgs == null || msgs.length == 0)
+            return;
+
+        String text = "";
+        //        String tagId = new String(msgs[0].getRecords()[0].getType());
+        byte[] payload = msgs[0].getRecords()[0].getPayload();
+        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16"; // Get the Text Encoding
+        int languageCodeLength = payload[0] & 0063; // Get the Language Code, e.g. "en"
+        // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+
+        try {
+            // Get the Text
+            text = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        } catch (UnsupportedEncodingException e) {
+            Log.e("UnsupportedEncoding", e.toString());
+        }
+        callbackContext.success(text);
+        // tvNFCContent.setText("NFC Content: " + text);
     }
 }
